124091995f8b42c25a02934a3260f121d4bcac62
Peter Vlk <petervlk86@gmail.com>
2023-06-22 19:53:26 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-chess/simple-chess
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simple_chess/simple_chess_test.clj
@@ -0,0 +1,7 @@
+(ns simple-chess.simple-chess-test
+  (:require [clojure.test :refer :all]
+            [simple-chess.simple-chess :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
